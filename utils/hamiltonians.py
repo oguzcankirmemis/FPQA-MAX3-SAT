2640a0704d4bb5f8fe86ec85112490c3f7ce2ddf
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2024-04-22 16:36:01 +0200

    Add 3SAT to Hamiltonian conversion

---
+++ utils/hamiltonians.py
@@ -0,0 +1,45 @@
+from pysat.formula import CNF
+import numpy as np
+
+class Max3satHamiltonian:
+    def __init__(self, file):
+        self.formula = CNF(from_file=file)
+        self.single_terms = np.zeros(self.formula.nv)
+        self.quadratic_terms = np.zeros((self.formula.nv, self.formula.nv))
+        self.cubic_terms = np.zeros((self.formula.nv, self.formula.nv, self.formula.nv))
+        for clause in self.formula.clauses:
+            variables = np.sort(np.abs(clause)) - 1
+            signs = np.sign([clause[i] for i in np.argsort(np.abs(clause))])
+            for i in range(len(variables)):
+                v_i = variables[i]
+                self.single_terms[v_i] += -signs[i]
+                for j in range(i + 1, len(variables)):
+                    v_j = variables[j]
+                    self.quadratic_terms[v_i][v_j] += -(signs[i] * signs[j])
+                    for k in range(j + 1, len(variables)):
+                        v_k = variables[k]
+                        self.cubic_terms[v_i][v_j][v_k] += -(signs[i] * signs[j] * signs[k])
+
+    def __pauli_string(self, indices):
+        pauli_string = ["I"] * self.formula.nv
+        for i in indices:
+            pauli_string[i] = "Z"
+        return "".join(pauli_string)
+
+    
+    def get_pauli_list(self):
+        pauli_list = []
+        for i in range(self.formula.nv):
+            if self.single_terms[i] != 0:
+                substring = self.__pauli_string([i])
+                pauli_list.append([self.single_terms[i], substring])
+            for j in range(i + 1, self.formula.nv):
+                if self.quadratic_terms[i][j] != 0:
+                    substring = self.__pauli_string([i, j])
+                    pauli_list.append([self.quadratic_terms[i][j], substring])
+                for k in range(j + 1, self.formula.nv):
+                    if self.cubic_terms[i][j][k] != 0:
+                        substring = self.__pauli_string([i, j, k])
+                        pauli_list.append([self.cubic_terms[i][j][k], substring])
+        return pauli_list
+                        
