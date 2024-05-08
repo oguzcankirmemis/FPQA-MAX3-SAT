4d517b9d734ab34da59d5847027144c86ed540b2
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2024-05-08 16:22:37 +0200

    Add utility for running superconducting experiments and add preliminary results for superconducting

---
+++ README.md
@@ -0,0 +1,3 @@
+# MAX3-SAT-FPQA
+
+SAT instances are taken from [SATLIB](https://www.cs.ubc.ca/~hoos/SATLIB/benchm.html) benchmrk set. 
--- utils/circuit_utils.py
+++ utils/circuit_utils.py
@@ -1,7 +1,7 @@
 from qiskit.converters import circuit_to_dag
 import numpy as np
 
-DESIRED_SUCCESS_PROBABILITY = 0.666
+DESIRED_SUCCESS_PROBABILITY = 0.6666
     
 def calculate_expected_fidelity(circuit, backend):
     dag = circuit_to_dag(circuit)
@@ -33,9 +33,22 @@ def calculate_expected_fidelity(circuit, backend):
             decoherence_fidelity *= t1 * t2                
     estimated_shots_without_decoherence = np.log(1 - DESIRED_SUCCESS_PROBABILITY) / np.log(1 - fidelity)
     estimated_shots = np.log(1 - DESIRED_SUCCESS_PROBABILITY) / np.log(1 - fidelity * decoherence_fidelity)
-    return fidelity * decoherence_fidelity, fidelity, int(np.ceil(estimated_shots)), int(np.ceil(estimated_shots_without_decoherence))
+    return fidelity * decoherence_fidelity, fidelity, estimated_shots, estimated_shots_without_decoherence
+
+def calculate_expected_fidelity_ideal(circuit, two_qubit_gate_fidelity=0.995, single_qubit_gate_fidelity=0.999):
+    dag = circuit_to_dag(circuit)
+    fidelity = 1
+    for gate in dag.gate_nodes():
+        if gate.name == "ecr":
+            fidelity *= two_qubit_gate_fidelity
+        elif gate.name == "measure" or gate.name == "barrier":
+            continue
+        else:
+            fidelity *= single_qubit_gate_fidelity  
+    estimated_shots = np.log(1 - DESIRED_SUCCESS_PROBABILITY) / np.log(1 - fidelity)
+    return fidelity, estimated_shots
 
 def calculate_swap_overhead(actual_circuit, transpiled_circuit):
     actual_ops = actual_circuit.count_ops()
     transpiled_ops = transpiled_circuit.count_ops()
-    return int((transpiled_ops["ecr"] - actual_ops["cz"]) / 3)
+    return int((transpiled_ops["ecr"] - actual_ops["cx"]) / 3)
