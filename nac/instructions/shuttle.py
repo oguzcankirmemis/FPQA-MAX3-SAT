cf7a107ab49a0d3b0ca533f0935a206fa907eb4d
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2024-07-03 15:41:40 +0200

    Implement shuttle instruction

--- nac/instructions/shuttle.py
+++ nac/instructions/shuttle.py
@@ -3,19 +3,58 @@ from nac.fpqa import FPQA
 
 class Shuttle(Instruction):
     def __init__(fpqa: FPQA, is_row: bool, index: int, offset: float):
-        pass
+        self.fpqa = fpqa
+        self.is_row = is_row
+        self.index = index
+        self.offset = offset
     
-    def apply():
-        pass
+    def apply(self):
+        if not self.verify():
+            raise ValueError("Cannot apply shuttle in current FPQA setting")
+        if self.is_row:
+            self.fpqa.aod.rows[self.index += offset
+        else:
+            self.fpqa.aod.cols[self.index] += offset
+    
+    def verify(self) -> bool:
+        if self.is_row():
+            return self._verify_row()
+        return self._verify_column()
 
-    def verify() -> bool:
-        pass
+    def _verify_row(self) -> bool:
+        next_pos = self.fpqa.aod.rows[self.index] + self.offset
+        if self.index > 0:
+            if self.fpqa.aod.rows[self.index - 1] > next_pos:
+                return False
+            if abs(self.fpqa.aod.rows[self.index - 1] - next_pos) < self.fpqa.config["AOD_BEAM_PROXIMITY"]:
+                return False
+        if self.index < len(self.fpqa.aod.rows) - 1:
+            if self.fpqa.aod.rows[self.index + 1] < next_pos:
+                return False
+            if abs(self.fpqa.aod.rows[self.index + 1] - next_pos) < self.fpqa.config["AOD_BEAM_PROXIMITY"]:
+                return False
+        return True
 
-    def qasm() -> str:
-        pass
+    def _verify_column(self) -> bool:
+        next_pos = self.fpqa.aod.cols[self.index] + self.offset
+        if self.index > 0:
+            if self.fpqa.aod.cols[self.index - 1] > next_pos:
+                return False
+            if abs(self.fpqa.aod.cols[self.index - 1] - next_pos) < self.fpqa.config["AOD_BEAM_PROXIMITY"]:
+                return False
+        if self.index < len(self.fpqa.aod.cols) - 1:
+            if self.fpqa.aod.cols[self.index + 1] < next_pos:
+                return False
+            if abs(self.fpqa.aod.cols[self.index + 1] - next_pos) < self.fpqa.config["AOD_BEAM_PROXIMITY"]:
+                return False
+        return True
+
+    def qasm(self) -> str:
+        row_or_col = "row" if self.is_row else "col"
+        return f"@shuttle {row_or_col} {self.index} {self.offset}"
 
     def avg_fidelity() -> float:
-        pass
+        return self.fpqa.config["SHUTTLING_FIDELITY"]
 
     def duration() -> float:
-        pass
+        return self.offset / self.fpqa.config["SHUTTLING_SPEED"]
