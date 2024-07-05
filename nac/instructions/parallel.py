8a9278c86766932d0a2d5e6d61cf784838608c8d
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2024-07-05 11:43:24 +0200

    Implement parallel instruction

---
+++ nac/instructions/parallel.py
@@ -0,0 +1,31 @@
+from nac.instructions.base import Instruction
+
+
+class Parallel(Instruction):
+    def __init__(self, instructions: list[Instruction]):
+        self.instructions = instructions
+    
+    def apply(self):
+        for instruction in self.instructions:
+            if not instruction.verify():
+                raise ValueError("Cannot apply parallel in current FPQA setting")
+            instruction.apply()
+
+    def verify(self) -> bool:
+        return all(instruction.verify() for instruction in self.instructions)
+
+    def qasm(self) -> str:
+        instruction_strs = ["@parallel begin\n", *[instruction.qasm() for instruction in self.instructions], "@parallel end\n"]
+        return "".join(instruction_strs)
+
+    def avg_fidelity(self) -> float:
+        fidelity = 1.0
+        for instruction in self.instructions:
+            fidelity *= instruction.avg_fidelity
+        return fidelity
+    
+    def duration() -> float:
+        duration = 0.0
+        for instruction in self.instructions:
+            duration = max(duration, instruction)
+        return duration
